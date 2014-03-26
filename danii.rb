94c869d9f098261adb830bcc5c88f14740558ad6
daniela <dalbazi@cps.edu>
2014-03-26 20:07:20 +0000

    second program

--- danii.rb
+++ danii.rb
@@ -1,8 +1,3 @@
-puts 'do u like the bulls?' 
-like = gets.chomp 
-if ( like == "yes")
-  puts 'Go Bulls' 
-else 
-  puts 'come back when you are a real basketball fan'
-end
-puts "please work!' 
+arrey ['English', 'french', 'spanish',] 
+var1 = arrey.length 
+puts 'i love english, dont know?' 
